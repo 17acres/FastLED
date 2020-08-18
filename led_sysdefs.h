aac0be18304078bf0aef1140079bb9f0d3d383b6
17acres <dan@17acr.com>
2020-08-18 18:24:22 -0400

    Initial Commit

--- led_sysdefs.h
+++ led_sysdefs.h
@@ -1,43 +1,44 @@
 #ifndef __INC_LED_SYSDEFS_H
 #define __INC_LED_SYSDEFS_H
 
-#include "FastLED.h"
-
-#include "fastled_config.h"
+#include <FastLED/fastled_config.h>
+#include <FastLED/FastLED.h>
 
 #if defined(NRF51) || defined(__RFduino__) || defined (__Simblee__)
-#include "platforms/arm/nrf51/led_sysdefs_arm_nrf51.h"
+#include <FastLED/platforms/arm/nrf51/led_sysdefs_arm_nrf51.h>
 #elif defined(NRF52_SERIES)
-#include "platforms/arm/nrf52/led_sysdefs_arm_nrf52.h"
+#include <FastLED/platforms/arm/nrf52/led_sysdefs_arm_nrf52.h>
 #elif defined(__MK20DX128__) || defined(__MK20DX256__)
 // Include k20/T3 headers
-#include "platforms/arm/k20/led_sysdefs_arm_k20.h"
+#include <FastLED/platforms/arm/k20/led_sysdefs_arm_k20.h>
 #elif defined(__MK66FX1M0__) || defined(__MK64FX512__)
 // Include k66/T3.6 headers
-#include "platforms/arm/k66/led_sysdefs_arm_k66.h"
+#include <FastLED/platforms/arm/k66/led_sysdefs_arm_k66.h>
 #elif defined(__MKL26Z64__)
 // Include kl26/T-LC headers
-#include "platforms/arm/kl26/led_sysdefs_arm_kl26.h"
+#include <FastLED/platforms/arm/kl26/led_sysdefs_arm_kl26.h>
 #elif defined(__IMXRT1062__)
 // teensy4
-#include "platforms/arm/mxrt1062/led_sysdefs_arm_mxrt1062.h"
+#include <FastLED/platforms/arm/mxrt1062/led_sysdefs_arm_mxrt1062.h>
 #elif defined(__SAM3X8E__)
 // Include sam/due headers
-#include "platforms/arm/sam/led_sysdefs_arm_sam.h"
+#include <FastLED/platforms/arm/sam/led_sysdefs_arm_sam.h>
 #elif defined(STM32F10X_MD) || defined(__STM32F1__)
-#include "platforms/arm/stm32/led_sysdefs_arm_stm32.h"
+#include <FastLED/platforms/arm/stm32/led_sysdefs_arm_stm32.h>
 #elif defined(__SAMD21G18A__) || defined(__SAMD21J18A__) || defined(__SAMD21E17A__) || defined(__SAMD21E18A__) || defined(__SAMD51G19A__) || defined(__SAMD51J19A__)
-#include "platforms/arm/d21/led_sysdefs_arm_d21.h"
+#include <FastLED/platforms/arm/d21/led_sysdefs_arm_d21.h>
 #elif defined(ESP8266)
-#include "platforms/esp/8266/led_sysdefs_esp8266.h"
+#include <FastLED/platforms/esp/8266/led_sysdefs_esp8266.h>
 #elif defined(ESP32)
-#include "platforms/esp/32/led_sysdefs_esp32.h"
+#include <FastLED/platforms/esp/32/led_sysdefs_esp32.h>
 #elif defined(__AVR__)
 // AVR platforms
-#include "platforms/avr/led_sysdefs_avr.h"
+#include <FastLED/platforms/avr/led_sysdefs_avr.h>
 #elif defined(ARDUINO_ARCH_APOLLO3)
 // Apollo3 platforms (e.g. the Ambiq Micro Apollo3 Blue as used by the SparkFun Artemis platforms)
-#include "platforms/apollo3/led_sysdefs_apollo3.h"
+#include <FastLED/platforms/apollo3/led_sysdefs_apollo3.h>
+#elif defined(TIVAWARE)
+#include <FastLED/platforms/arm/tirtos/led_sysdefs_tirtos.h>
 #else
 //
 // We got here because we don't recognize the platform that you're
---
+++ platforms/arm/tirtos/fastled_arm_tirtos.h
@@ -0,0 +1,8 @@
+#ifndef __INC_FASTLED_ARM_TIRTOS_H
+#define __INC_FASTLED_ARM_TIRTOS_H
+
+// Include the tirtos headers
+#include <FastLED/platforms/arm/tirtos/fastpin_arm_tirtos.h>
+#include <FastLED/platforms/arm/tirtos/fastspi_arm_tirtos.h>
+
+#endif
